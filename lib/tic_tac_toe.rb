d9a2f04ff2b5338671afc4882061d108a172da4d
Alex Ines <lex@blackcat.watch>
2020-05-07 23:43:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,14 +1,14 @@
 class TicTacToe
 
   WIN_COMBINATIONS = [
-    [0,1,2], # Top row
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ]
 
   def initialize
@@ -23,20 +23,12 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(user_input)
-    user_input.to_i - 1
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
   end
 
-  def move(index, current_player = "X")
-    @board[index] = current_player
-  end
-
-  def position_taken?(location)
-    unless @board[location] = " " && @board[location].nil?
-  end
-
-  def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index)
+  def full?
+    @board.flatten.all?{ |el| el=="X" || el=="O" }
   end
 
   def won?
@@ -71,27 +63,58 @@ class TicTacToe
   def turn_count
     count = 0
     @board.each do |element|
-       if element == "X" || element =="O"
-         count += 1
-       end
+      if element == "X" || element =="O"
+        count += 1
+      end
     end
     count
   end
 
+def current_player
+  if turn_count % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
 
-  # def full?
-  #   @board.flatten.all?{ |el| el=="X" || el=="O" }
-  # end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-  # def turn
-  #   puts "Your move: "
-  #   user_choice = input_to_index(gets.strip)
-  #   until valid_move?(user_choice)
-  #     puts "Pls enter new, valid position"
-  #     user_choice = input_to_index(gets.strip)
-  #   end
-  #   move(user_choice, current_player)
-  #   display_board
-  # end
+def move(index, current_player)
+  @board[index] = current_player
+end
 
+def position_taken?(location)
+  @board[location] != " " && @board[location] != ""
+end
+
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+
+def turn
+  puts "Your move: "
+  user_choice = input_to_index(gets.strip)
+  until valid_move?(user_choice)
+    puts "Pls enter new, valid position"
+    user_choice = input_to_index(gets.strip)
+  end
+  move(@board, user_choice, current_player)
+  display_board
+  @board
+end
+
+# Define your play method below
+def play
+    until over?|| draw?
+      @board = turn
+    end
+    unless winner.nil?
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
 end
